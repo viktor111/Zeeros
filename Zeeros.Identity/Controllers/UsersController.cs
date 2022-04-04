91bb6213d02f0ee518c7b6bb2e2c49451767ef98
Viktor Draganov <viktor.draganov@elando.com>
2022-04-04 11:19:41 +0300

    s

--- Zeeros.Identity/Controllers/UsersController.cs
+++ Zeeros.Identity/Controllers/UsersController.cs
@@ -6,6 +6,50 @@ namespace Zeeros.Identity.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        
+        private readonly IUserRepository _userRepository;
+        private readonly ITokenService _tokenService;
+        private readonly IPasswordService _passwordService;
+        private readonly IConfiguration _configuration;
+        private readonly IRegisterService _registerService;
+
+        public UsersController(IUserRepository userRepository,
+            ITokenService tokenService,
+            IPasswordService passwordService,
+            IConfiguration configuration,
+            IRegisterService registerService)
+        {
+            _userRepository = userRepository;
+            _tokenService = tokenService;
+            _passwordService = passwordService;
+            _configuration = configuration;
+            _registerService = registerService;
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Authenticate(AuthenticateUserDto authenticateUserDto)
+        {
+            var user = await _userRepository.GetUserByUsername(authenticateUserDto.UserName);
+
+            if (user is not null)
+            {
+                bool res = _passwordService.Verify(authenticateUserDto.Password, user.PasswordHash);
+                if (res)
+                {
+                    var jwt = _tokenService.Issue(user, _configuration);
+                    return Ok(jwt);
+                }
+            }
+
+            return NotFound("Incorrect phone number or password");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterUserDto registerUserDto)
+        {
+            var createdUser = await _registerService.RegisterNewUser(registerUserDto);
+
+            return Ok(createdUser);
+        }
     }
 }
---
+++ Zeeros.Identity/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Zeeros.Identity.Data.Repositories;
+using Zeeros.Identity.Service;
+
+namespace Zeeros.Identity.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddServices(
+            this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPasswordService, PasswordService>();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IRegisterService, RegisterService>();
+
+            return services;
+        }
+    }
+}
